d981dfd489c6dc1374a140dd966e00b85d4fae04
Jake Territo <jake@findanomaly.com>
2025-12-01 22:35:02 -0500

    mostly not working

--- 2025/1/main.go
+++ 2025/1/main.go
@@ -17,32 +17,47 @@ func getRotationValue(rotation string) int {
 	return value
 }
 
-const maxPosition = 99
+const dialSize = 100
 
-func turnDial(position, increment int) (newPosition, zeros int) {
-	newPosition = position + increment
+func turnDial(position, increment int) (newPosition, passZeroCount int) {
+	newPosition = (position + increment) % (dialSize)
+	if newPosition < 0 {
+		newPosition = dialSize + newPosition
+	}
 
-	zeros = (newPosition - position)
+	passZeroCount = increment / dialSize
+	if passZeroCount < 0 {
+		passZeroCount = -passZeroCount
+	}
 
-	newPosition = newPosition % (maxPosition + 1)
+	if increment > 0 && newPosition < position {
+		passZeroCount += 1
+	} else if increment < 0 && newPosition > position {
+		passZeroCount += 1
+	}
+	return newPosition, passZeroCount
 
-	
-	return newPosition, zeros
 }
 
 const initialPosition = 50
 
-func analyzeRotationsPart1(rotations []string) (password int) {
+func analyzeRotations(rotations []string) (passwordPart1, passwordPart2 int) {
 	position := initialPosition
+	var passZeroCount int
 	for _ , rotation := range rotations {
-		position, _ = turnDial(position, getRotationValue(rotation))
+		position, passZeroCount = turnDial(position, getRotationValue(rotation))
+		
 		if position == 0 {
-			password += 1
+			passwordPart1 += 1
 		}
+		passwordPart2 += passZeroCount
+
+		fmt.Printf("%s -> %v\n", rotation, position)
 	}
 
-	return password
+	return passwordPart1, passwordPart2
 }
+
 func main() {
 	file, _ := os.Open("./2025/1/input.txt")
 	defer file.Close()
@@ -53,9 +68,10 @@ func main() {
     	input = append(input, scanner.Text())
 	}
 
-	passwordPart1 := analyzeRotationsPart1(input)
+	passwordPart1, passwordPart2 := analyzeRotations(input)
 
-	fmt.Printf("Password for part 1 is %v", passwordPart1)
+	fmt.Printf("Password for part 1 is %v\n", passwordPart1)
+	fmt.Printf("Password for part 2 is %v\n", passwordPart2)
 }
 
 
--- 2025/1/main_test.go
+++ 2025/1/main_test.go
@@ -5,28 +5,114 @@ import (
 )
 
 func TestGetRotationValue(t *testing.T) {
-	input := "R32"
-	expected := 32
-	result := getRotationValue(input)
+	tests := []struct {
+		name string
+		input string
+		expected int
+	}{
+		{
+			name: "Positive",
+			input: "R32",
+			expected: 32,
+		},
+		{
+			name: "Negative",
+			input: "L1234567",
+			expected: -1234567,
+		},
+	}
 
-	if result != expected {
-		t.Errorf("Result was incorrect for input %v. Expected: %v, Got: %v", input, expected, result)
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result := getRotationValue(tc.input)
+			if result != tc.expected {
+				t.Errorf("Result was incorrect for input %v. Expected: %v, Got: %v", tc.input, tc.expected, result)
+			}
+		})
 	}
 }
+
 
 func TestTurnDial(t *testing.T) {
-	start := 99
-	input := 1
-	expected := 0
-	result, _ := turnDial(start, input)
-	if result != expected {
-		t.Errorf("Result was incorrect for input %v. Expected: %v, Got: %v", input, expected, result)
+	tests := []struct {
+		name string
+		start int
+		input int
+		expectedPos int
+		expectedZeros int
+	}{
+		{
+			name:"A",
+			start: 50,
+			input: -50,
+			expectedPos: 0,
+			expectedZeros: 0,
+		},
+		{
+			name: "B",
+			start: 99,
+			input: 1,
+			expectedPos: 0,
+			expectedZeros: 1,
+		},
+		{
+			name: "C",
+			start: 50,
+			input: -64,
+			expectedPos: 86,
+			expectedZeros: 1,
+		},
+		{
+			name: "D",
+			start: 0,
+			input: -1001,
+			expectedPos: 99,
+			expectedZeros: 11,
+		},
+		{
+			name: "E",
+			start: 99,
+			input: 1001,
+			expectedPos: 0,
+			expectedZeros: 11,
+		},
+		{
+			name: "F",
+			start: 50,
+			input: 250,
+			expectedPos: 0,
+			expectedZeros: 3,
+		},
+	}
+	
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			pos, zeros := turnDial(tc.start, tc.input)
+			if pos != tc.expectedPos || zeros != tc.expectedZeros {
+				t.Errorf("Expected (%d, %d), got (%d, %d)",
+					tc.expectedPos, tc.expectedZeros, pos, zeros)
+			}
+		})
 	}
 }
 
-func TestTurnDialZeros(t *testing.T) {
-	start := 1
-	input := 99
+func TestTurnDialA(t *testing.T) {
+	start := 50
+	input := -50
+	expectedPosition, expectedZeros := 0 , 0
+	
+	resultPosition, resultZeros := turnDial(start, input)
+
+	if resultPosition != expectedPosition ||  resultZeros != expectedZeros {
+		t.Errorf("Result was incorrect for input %v. Expected: %v - %v, Got: %v - %v", input, expectedPosition, expectedZeros, resultPosition, resultZeros)
+	}
+}
+
+func TestTurnDialB(t *testing.T) {
+	start := 99
+	input := 1
 	expectedPosition, expectedZeros :=  0, 1
 
 	resultPosition, resultZeros := turnDial(start, input)
@@ -34,16 +120,36 @@ func TestTurnDialZeros(t *testing.T) {
 	if resultPosition != expectedPosition ||  resultZeros != expectedZeros {
 		t.Errorf("Result was incorrect for input %v. Expected: %v - %v, Got: %v - %v", input, expectedPosition, expectedZeros, resultPosition, resultZeros)
 	}
-
 }
 
-func TestAnalyzeRotationsPart1(t *testing.T) {
-	input := []string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"}
-	expected := 3
 
-	result := analyzeRotationsPart1(input)
-
-	if result != expected {
-		t.Errorf("Result was incorrect for input %s. Expected: %v, Got: %v", input, expected, result)	
+func TestAnalyzeRotations(t *testing.T) {
+	tests := []struct {
+		name string
+		input []string
+		expectedPart1 int
+		expectedPart2 int
+	}{
+		{
+			name:"A",
+			input: []string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"},	
+			expectedPart1: 3,
+			expectedPart2: 6,
+		},
+		{
+			name: "B",
+			input: []string{"L774"},
+			expectedPart1: 0,
+			expectedPart2: 8,
+		},
+	}
+	
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			resultPart1, resultPart2 := analyzeRotations(tc.input)
+			if resultPart1 != tc.expectedPart1 || resultPart2 != tc.expectedPart2 {
+				t.Errorf("Result was incorrect for input %s. Expected: %v - %v, Got: %v - %v", tc.input, tc.expectedPart1, tc.expectedPart2, resultPart1, resultPart2)	
+			}
+		})
 	}
 }
